a9137b3acd10c75c728a902e483344f21c1da936
giovanni <giacoletti92@gmail.com>
2022-03-27 20:50:15 +0200

    Add end2end test to check if visitor can click on slideshow indicator buttons to change slides

---
+++ cypress/integration/visitorCanChangeSlideInSlideshow.feature.js
@@ -0,0 +1,24 @@
+describe("A visitor can change slide of the slideshow by clicking indicator buttons", () => {
+  before(() => {
+    cy.intercept(
+      "GET",
+      "https://webshop.wm3.se/api/v1/shop/products?media_file=true"
+    );
+    cy.visit("/");
+    cy.get("[aria-label='Slide 2']").click();
+  });
+
+  it("is expected to see second image of the slideshow", () => {
+    cy.get("[data-cy=slide-2-image]").should("be.visible");
+  });
+
+  describe("can click on third indicator button", () => {
+    before(() => {
+      cy.get("[aria-label='Slide 3']").click();
+    });
+
+    it("is expected to see third image of the slideshow", () => {
+      cy.get("[data-cy=slide-3-image]").should("be.visible");
+    });
+  });
+});
